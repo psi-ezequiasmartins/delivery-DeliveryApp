033cefbc8e629dbcad87b0a9e1225307d0fd25c9
Ezequias Martins <ezequiasmartins@gmail.com>
2022-09-08 14:32:57 -0300

    08-09-2022 14:33 src/app/menu/clientes

--- src/app/menu/clientes/index.jsx
+++ src/app/menu/clientes/index.jsx
@@ -4,7 +4,6 @@ import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 import { Impressao } from './impressao';
 import { Link, Redirect } from 'react-router-dom';
-import Listagem from './listagem.jsx';
 import MenuApp from '../menuapp.jsx';
 import './index.css';
 
@@ -16,6 +15,7 @@ function Index() {
 
     const [clientes, setClientes] = useState([]);
 
+    const [id_cliente, setIdCliente] = useState(null);
     const [nome, setNome] = useState('');
     const [email, setEmail] = useState('');
     const [telefone, setTelefone] = useState('');
@@ -36,6 +36,25 @@ function Index() {
 
     const [success, setSuccess] = useState('N');
     const [msg, setMsg] = useState('');
+
+    useEffect(() => {
+      let listagem = []; 
+      api.get('/clientes').then(async result => {
+        result.data.forEach(doc => {
+          if (doc.nome.indexOf(busca) >=0 ) {
+            listagem.push({
+              id_cliente: doc.id_cliente,
+              nome: doc.nome,
+              telefone: doc.telefone,
+              email: doc.email,
+              bairro: doc.bairro
+            })
+          }
+        })
+        setClientes(listagem);
+        console.log(listagem);
+      })
+    }, [busca, excluido, success]);
 
     function Cadastrar() {
       if (nome.length === 0) {
@@ -67,6 +86,42 @@ function Index() {
       }
     }
 
+    async function Editar() {
+      if (nome.length === 0) {
+        setMsg('Favor preencher o campo Nome do Cliente.');
+      } else if (email.length === 0) {
+        setMsg('Favor preencher o campo E-mail.');
+      } else {
+        const json = { 
+          "id_cliente": id_cliente,
+          "nome": nome, "email": email, "telefone": telefone,
+          "endereco": endereco, "complemento": complemento, "bairro": bairro,  "cidade": cidade, "uf": UF, "cep": CEP,
+          "cnpj": cnpj, "cpf": cpf
+        }
+        await api.put(`/cliente/update/${id_cliente}`, json).then(response => {
+          console.log(response.data);
+        }).then(() => {
+          setMsg('');
+          setSuccess('S');
+        }).catch((erro) => {
+          setMsg(erro);
+          setSuccess('N');
+        })
+      }
+    }
+
+    function selectById(id){
+      api.get(`/cliente/${id}`)
+      .then(result => {
+        setIdCliente(result.data[0].id_cliente);
+        setNome(result.data[0].nome); 
+        setEmail(result.data[0].email); setTelefone(result.data[0].telefone);
+        setEndereco(result.data[0].endereco); setComplemento(result.data[0].complemento); setBairro(result.data[0].bairro);
+        setCidade(result.data[0].cidade); setUf(result.data[0].uf); setCep(result.data[0].cep);
+        setCnpj(result.data[0].cnpj); setCpf(result.data[0].cpf);
+      })
+    }
+
     function deleteByID(id) {
       api.delete(`/cliente/delete/${id}`).then(async(result) => {
       setExcluido(id);
@@ -80,31 +135,48 @@ function Index() {
       setConfirmaId(id);
       setConfirma(true);
     }
-  
-    useEffect(() => {
-      let listagem = []; 
-      api.get('/clientes').then(async result => {
-        result.data.forEach(doc => {
-          if (doc.nome.indexOf(busca) >=0 ) {
-            listagem.push({
-              id_cliente: doc.id_cliente,
-              nome: doc.nome,
-              telefone: doc.telefone,
-              email: doc.email,
-              bairro: doc.bairro
-            })
-          }
-        })
-        setClientes(listagem);
-        console.log(listagem);
-      })
-    }, [busca, excluido, success]);
 
     const VisualizarPDF = async () => {
       console.log('report', clientes);
       const classeImpressao = new Impressao(clientes);
       const documento = await classeImpressao.PreparaDocumento();
       pdfMake.createPdf(documento).open({}, window.open('', '_blank'));
+    }
+
+    function Listagem(props) {
+      return (
+        <table className="table table-hover table-bordered">
+          <thead>
+            <tr className="table-secondary">
+              <th scope="col">ID</th>
+              <th scope="col">Cliente</th>
+              <th scope="col">Telefone</th>
+              <th scope="col">E-mail</th>
+              <th scope="col">Bairro</th>
+              <th scope="col" className="col-action"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {  
+              props.array.map((cliente) => {
+                return (
+                  <tr key={cliente.id_cliente}>
+                    <th scope="row">{cliente.id_cliente}</th>
+                    <td>{cliente.nome}</td>
+                    <td>{cliente.telefone}</td>
+                    <td>{cliente.email}</td>
+                    <td>{cliente.bairro}</td>
+                    <td>
+                      <Link to="#" onClick={()=>props.select(cliente.id_cliente)} title="EDITAR CLIENTE" data-bs-toggle="modal" data-bs-target="#md_editarcliente"><i className="fas fa-user-edit icon-action"></i></Link>
+                      <Link to="#" onClick={()=>props.delete(cliente.id_cliente)} title="EXCLUIR CLIENTE"><i className="fas fa-trash-alt icon-action red"></i></Link>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      );
     }
 
     return (
@@ -131,7 +203,7 @@ function Index() {
             </div>
           </div>
           
-          <Listagem array={clientes} clickDelete={confirmaExclusao} />
+          <Listagem array={clientes} select={selectById} delete={confirmaExclusao} />
           
           {
             confirma ? <SweetAlert
@@ -252,6 +324,116 @@ function Index() {
               <div className="modal-footer">
                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">CANCELAR</button>
                 <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={Cadastrar}>SALVAR</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* -- md_editarcliente -- */}
+        <div className="modal fade" id="md_editarcliente" tabindex="-1" aria-labelledby="titulo_modal" aria-hidden="true">
+          <div className="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+            <div className="modal-content">
+
+              <div className="modal-header">
+                <h5 className="modal-title" id="titulo_modal">EDITAR CLIENTE</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
+              </div>
+
+              <div className="modal-body">
+                <form>
+
+                  <div className="mb-2">
+                    <label htmlFor="nome" className="form-label">Nome do Cliente</label>
+                    <input onChange={e => setNome(e.target.value)} value={nome} type="text" className="form-control" id="nome" />
+                  </div>
+
+                  <div className="row mb-2">
+                    <div className="col-sm-8">
+                      <label htmlFor="email" className="form-label">E-mail</label>
+                      <input onChange={e => setEmail(e.target.value)} value={email} type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="col-sm-4">
+                      <label htmlFor="telefone" className="form-label">Telefone</label>
+                      <input onChange={e => setTelefone(e.target.value)} value={telefone} type="text" className="form-control" id="telefone" />
+                    </div>
+                  </div>
+
+                  <div className="row mb-2">
+                    <div className="col-sm-6">
+                      <label htmlFor="endereco" className="form-label">Endereço</label>
+                      <input onChange={e => setEndereco(e.target.value)} value={endereco} type="text" className="form-control" id="endereco" />
+                    </div>
+                    <div className="col-sm-3">
+                      <label htmlFor="complemento" className="form-label">Complemento</label>
+                      <input onChange={e => setComplemento(e.target.value)} value={complemento} type="text" className="form-control" id="complemento" />
+                    </div>
+                    <div className="col-sm-3">
+                      <label htmlFor="bairro" className="form-label">Bairro</label>
+                      <input onChange={e => setBairro(e.target.value)} value={bairro} type="text" className="form-control" id="bairro" />
+                    </div>
+                  </div>
+
+                  <div className="row mb-2">
+                    <div className="col-sm-5">
+                      <label htmlFor="cidade" className="form-label">Cidade</label>
+                      <input onChange={e => setCidade(e.target.value)} value={cidade} type="text" className="form-control" id="cidade" />
+                    </div>
+                    <div className="col-sm-4">
+                      <label htmlFor="UF" className="form-label">UF</label>
+                      <select onChange={e => setUf(e.target.value)} value={UF} class="form-select" id="UF">
+                        <option value="AC">ACRE</option>
+                        <option value="AL">ALAGOAS</option>
+                        <option value="AP">AMAPA</option>
+                        <option value="AM">AMAZONAS</option>
+                        <option value="BA">BAHIA</option>
+                        <option value="CE">CEARA</option>
+                        <option value="DF">DISTRITO FEDERAL</option>
+                        <option value="ES">ESP.SANTO</option>
+                        <option value="GO">GOIAS</option>
+                        <option value="MA">MARANHAO</option>
+                        <option value="MT">MATO GROSSO</option>
+                        <option value="MS">MATO GROSSO SUL</option>
+                        <option value="MG" selected="selected">MINAS GERAIS</option>
+                        <option value="PA">PARA</option>
+                        <option value="PB">PARAIBA</option>
+                        <option value="PR">PARANA</option>
+                        <option value="PE">PERNAMBUCO</option>
+                        <option value="PI">PIAUI</option>
+                        <option value="RJ">RIO DE JANEIRO</option>
+                        <option value="RN">RIO GRANDE DO NORTE</option>
+                        <option value="RS">RIO GRANDE DO SUL</option>
+                        <option value="RO">RONDONIA</option>
+                        <option value="RR">RORAIMA</option>
+                        <option value="SC">SANTA CATARINA</option>
+                        <option value="SP">SAO PAULO</option>
+                        <option value="SE">SERGIPE</option>
+                        <option value="TO">TOCANTINS</option>
+                      </select>
+                    </div>
+                    <div className="col-sm-3">
+                      <label htmlFor="CEP" className="form-label">CEP</label>
+                      <input onChange={e => setCep(e.target.value)} value={CEP} type="text" className="form-control" id="CEP" />
+                    </div>
+                  </div>
+
+                  <div className="row mb-2">
+                    <div className="col-sm-6">
+                      <label htmlFor="cnpj" className="form-label">CNPJ</label>
+                      <input onChange={e => setCnpj(e.target.value)} value={cnpj} type="text" className="form-control" id="cnpj" />
+                    </div>
+                    <div className="col-sm-6">
+                      <label htmlFor="cpf" className="form-label">CPF</label>
+                      <input onChange={e => setCpf(e.target.value)} value={cpf} type="text" className="form-control" id="cpf" />
+                    </div>
+                  </div>
+
+                  {msg.length > 0 ? <div className="alert alert-danger mt-2" role="alert">{msg}</div> : null}
+                  {success === 'S' ? <Redirect to='/app/menu/clientes'/> : null}
+                </form>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">CANCELAR</button>
+                <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={Editar}>SALVAR</button>
               </div>
             </div>
           </div>
