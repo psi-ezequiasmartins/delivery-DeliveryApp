b075243c3e2b01f6672159dc356ae7a8cf2ed619
Ezequias <ezequiasmartins@gmail.com>
2023-07-15 13:58:40 -0300

    15072023-1358

--- src/site/pages/banner.jsx
+++ src/site/pages/banner.jsx
@@ -13,7 +13,7 @@ function Banner() {
               <div className="bloco">
                 <h1>Simples e fácil de usar!</h1>
                 <h4>Gestão de Pedidos e Cardápio de Produtos para Bares, Restaurantes, Fast-foods e Delivery's em Geral. Tudo isso em um único lugar! Além da divulgação do seu Cardápio em nosso aplicativo para Mobile (Android/iOs).</h4>
-                <a href="/app/login/novo" type="button" className="btn btn-primary btn-lg btn-app">Criar uma conta</a>
+                <a href="#app/login/novo" type="button" className="btn btn-primary btn-lg btn-app">Criar uma conta</a>
                 <a href="/app/pedidos" type="button" className="btn btn-outline-light btn-lg btn-app">Fazer Login</a>
               </div>
             </div>
