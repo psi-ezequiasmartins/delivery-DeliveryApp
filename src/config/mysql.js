be000aef5311f63ca483d5431546793d0faaed05
Ezequias <ezequiasmartins@gmail.com>
2023-07-31 20:48:23 -0300

    31072023-2048

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
-const URL = "http://localhost:3333"; // teste local (31072023-2037)
-// const URL = "https://srv.deliverybairro.com";
+// const URL = "http://localhost:3333"; // teste local (31072023-2037)
+const URL = "https://srv.deliverybairro.com";
 
 const api = Axios.create({
   baseURL: URL,
