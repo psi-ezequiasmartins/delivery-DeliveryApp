483649c6a3a1864a298001516ebf509cb5783a04
Ezequias <ezequiasmartins@gmail.com>
2023-08-13 17:27:16 -0300

    13082023-1727

--- src/site/pages/depoimentos.jsx
+++ src/site/pages/depoimentos.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Depoimentos() {
   return (
     <section id="depoimentos">
-      <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+      <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel" data-mdb-interval="true" >
 
         <center>
             <h1>Depoimentos</h1>
@@ -12,22 +12,22 @@ function Depoimentos() {
         <div className="carousel-inner">
           <div className="carousel-item active" data-bs-interval="5000">
             <img src="images/heliomarmarques.jpg" alt="Heliomar P. Marques"/><br/>
-            <em>Heliomar P Marques - São Paulo</em>  
+            <em>Heliomar P Marques - São Paulo</em>
             <p>Excelente ferramenta para administrar seu Delivery no dia a dia.</p>
           </div>
           <div className="carousel-item" data-bs-interval="5000">
             <img src="images/afonsomotta.jpg" alt="Afonso Motta"/><br/>
-            <em>Afonso Motta - Rio de Janeiro</em>  
+            <em>Afonso Motta - Rio de Janeiro</em>
             <p>Design simples e elegante, interface intuitiva e agradável para todos os usuários.</p>
           </div>
           <div className="carousel-item" data-bs-interval="5000">
             <img src="images/ezequiasmartins.jpg" alt="Ezequias Martins"/><br/>
-            <em>Ezequias Martins - Belo Horizonte</em>  
+            <em>Ezequias Martins - Belo Horizonte</em>
             <p>O DeliveryBairro é uma ferramenta útil e intuitiva para gestão de Delivery's em geral.</p>
           </div>
           <div className="carousel-item" data-bs-interval="5000">
             <img src="images/sergiosales.jpg" alt="Sérgio Sales"/><br/>
-            <em>Sérgio Sales - Belo Horizonte</em>  
+            <em>Sérgio Sales - Belo Horizonte</em>
             <p>O DeliveryBairro reduz os custos e a distância entre você e seus clientes.</p>
           </div>
         </div>
