fdfa23da8e935d84352530cefb36a6a53619a13c
Ezequias <ezequiasmartins@gmail.com>
2024-05-30 13:27:24 -0300

    30052024-1327

--- src/app/pedidos/pedido.jsx
+++ src/app/pedidos/pedido.jsx
@@ -1,79 +1,84 @@
-/**
- * src/app/peidos/pedido.jsx
- */
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import api from '../../config/mysql';
-
 import './pedido.css';
 
-export default function Pedido(props){
+export default function Pedido(props) {
   const [status, setStatus] = useState(props.Status);
   const [visible, setVisible] = useState(true);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth <= 768);
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   async function AlterarStatus(codigo) {
     setStatus(codigo);
-    await api.put(`/update/status/pedido/${props.PedidoID}`, {status: codigo}).then((response) => {
-      console.log(response);
-      sendPushNotification(props.TokenSMS, props.PedidoID, status);
-      if (status === "FINALIZADO") {
-        setVisible(false)
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    await api.put(`/update/status/pedido/${props.PedidoID}`, { status: codigo })
+      .then((response) => {
+        console.log(response);
+        sendPushNotification(props.TokenSMS, props.PedidoID, status);
+        if (status === 'FINALIZADO') {
+          setVisible(false);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
-
-  // Send Push Notification
 
   async function sendPushNotification(expoPushToken, pedido_id, codigo_status) {
     const message = {
       to: expoPushToken,
-      sound: "default",
-      title: "DeliveryBairro.com",
-      body: "Pedido #"+pedido_id+" atualizado em "+new Date().toLocaleString()+" Status: "+codigo_status,
+      sound: 'default',
+      title: 'DeliveryBairro.com',
+      body: 'Pedido #' + pedido_id + ' atualizado em ' + new Date().toLocaleString() + ' Status: ' + codigo_status,
       data: {
-        "PedidoID": pedido_id, 
-        "Status": codigo_status
-      }
+        PedidoID: pedido_id,
+        Status: codigo_status,
+      },
     };
 
-    await fetch("https://exp.host/--/api/v2/push/send", {
-      method: "POST",
-      referrerPolicy: "strict-origin-when-cross-origin",
-      mode: "no-cors",
+    await fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      referrerPolicy: 'strict-origin-when-cross-origin',
+      mode: 'no-cors',
       headers: {
-        "Accept": "*",
-        "Accept-encoding": "gzip, deflate",
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json"
+        Accept: '*',
+        'Accept-encoding': 'gzip, deflate',
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(message),
     });
   }
 
-  return !visible ? <></> : 
-  <div className='d-flex justify-content-between shadow-sm pedido'>
-    <div>
-      <span><b>Pedido #{props.PedidoID}</b></span>
-      <span className='badge rouded-pill bg-dark ms-2'>{props.Data}</span>
+  return !visible ? null : (
+    <div className={`shadow-sm pedido ${isMobile ? 'flex-column' : 'flex-row justify-content-between'}`}>
+      <div>
+        <span><b>Pedido #{props.PedidoID}</b></span>
+        <span className='badge bg-dark ms-2'>{props.Data}</span>
 
-      {status === "NOVO" ? <span className="badge rouded-pill bg-danger ms-2">NOVO</span> : null}
-      {status === "AGUARDANDO" ? <span className="badge rouded-pill bg-warning ms-2">AGUARDANDO</span> : null}
-      {status === "PREPARANDO" ? <span className="badge rouded-pill bg-primary ms-2">PREPARANDO</span> : null}
-      {status === "PRONTO_PARA_RETIRADA" ? <span className="badge rouded-pill bg-success ms-2">PRONTO PARA RETIRADA</span> : null}
-      {status === "SAIU_PARA_ENTREGA" ? <span className="badge rouded-pill bg-warning ms-2">SAIU PARA ENTREGA</span> : null}
-      {status === "RECEBIDO" ? <span className="badge rouded-pill bg-dark ms-2">RECEBIDO</span> : null}
-      {status === "FINALIZADO" ? <span className="badge rouded-pill bg-secondary ms-2">FINALIZADO</span> : null}
-      {status === "CANCELADO" ? <span className="badge rouded-pill bg-secondary ms-2">CANCELADO</span> : null}
+        {status === 'NOVO' && <span className='badge bg-danger ms-2'>NOVO</span>}
+        {status === 'AGUARDANDO' && <span className='badge bg-warning ms-2'>AGUARDANDO</span>}
+        {status === 'PREPARANDO' && <span className='badge bg-primary ms-2'>PREPARANDO</span>}
+        {status === 'PRONTO_PARA_RETIRADA' && <span className='badge bg-success ms-2'>PRONTO PARA RETIRADA</span>}
+        {status === 'SAIU_PARA_ENTREGA' && <span className='badge bg-warning ms-2'>SAIU PARA ENTREGA</span>}
+        {status === 'RECEBIDO' && <span className='badge bg-dark ms-2'>RECEBIDO</span>}
+        {status === 'FINALIZADO' && <span className='badge bg-secondary ms-2'>FINALIZADO</span>}
+        {status === 'CANCELADO' && <span className='badge bg-secondary ms-2'>CANCELADO</span>}
 
-      <small className='d-block mt-1 text-dark'>{props.Cliente} - {props.Endereco}</small>
-      <small className='d-block mt-1 text-secondary'>{props.TokenSMS}</small>
-      {
-        props.itens.map((item) => {
-          return <div className='d-inline-block align-items-start'>
-            <div className='text-left me-4 mt-2 card' key={item.ItemID} >
+        <small className='d-block mt-1 text-dark'>{props.Cliente} - {props.Endereco}</small>
+        <small className='d-block mt-1 text-secondary'>{props.TokenSMS}</small>
+        {props.itens.map((item) => (
+          <div className='d-inline-block align-items-start' key={item.ItemID}>
+            <div className='text-left me-4 mt-2 card'>
               <img src={item.UrlImagem} className='foto-item' alt='' />
               <small className='d-block text-dark'><b>({item.Qtd}x) {item.Produto}</b></small>
               {item.Acrescimos && (
@@ -88,84 +93,34 @@ export default function Pedido(props){
                   </ul>
                 </div>
               )}
-
               {item.Obs && (
                 <div className='text-danger obs'>
-                  <b>Observações:</b><br/>
+                  <b>Observações:</b><br />
                   {item.Obs}
                 </div>
               )}
             </div>
-
           </div>
-        })
-      }
-    </div>
+        ))}
+      </div>
 
-    <div className="d-flex me-4" id="status">
-      <div className="dropdown">
-        <a className="btn btn-dark dropdown-toggle" href="#status" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-          STATUS <i class="bi bi-pin-angle"></i>
-        </a>
-        <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-          <li><a href="#novo" onClick={(e)=>AlterarStatus("NOVO")} className="dropdown-item">Novo</a></li>
-          <li><a href="#aguardando" onClick={(e)=>AlterarStatus("AGUARDANDO")} className="dropdown-item">Aguardando</a></li>
-          <li><a href="#preparando" onClick={(e)=>AlterarStatus("PREPARANDO")} className="dropdown-item">Preparando</a></li>
-          <li><a href="#retirada" onClick={(e)=>AlterarStatus("PRONTO_PARA_RETIRADA")} className="dropdown-item">Pronto para retirada</a></li>
-          <li><a href="#entrega" onClick={(e)=>AlterarStatus("SAIU_PARA_ENTREGA")} className="dropdown-item">Saiu para Entrega</a></li>
-          <li><a href="#recebido" onClick={(e)=>AlterarStatus("RECEBIDO")} className="dropdown-item">Recebido</a></li>
-          <li><a href="#finalizado" onClick={(e)=>AlterarStatus("FINALIZADO")} className="dropdown-item">Finalizado</a></li>
-          <li><a href="#cancelado" onClick={(e)=>AlterarStatus("CANCELADO")} className="dropdown-item">Cancelado</a></li>
-        </ul>
+      <div className='d-flex me-4' id='status'>
+        <div className='dropdown'>
+          <a className='btn btn-dark dropdown-toggle' href='#status' role='button' id='dropdownMenuLink' data-bs-toggle='dropdown' aria-expanded='false'>
+            STATUS <i className='bi bi-pin-angle'></i>
+          </a>
+          <ul className='dropdown-menu' aria-labelledby='dropdownMenuLink'>
+            <li><a href='#novo' onClick={() => AlterarStatus('NOVO')} className='dropdown-item'>Novo</a></li>
+            <li><a href='#aguardando' onClick={() => AlterarStatus('AGUARDANDO')} className='dropdown-item'>Aguardando</a></li>
+            <li><a href='#preparando' onClick={() => AlterarStatus('PREPARANDO')} className='dropdown-item'>Preparando</a></li>
+            <li><a href='#retirada' onClick={() => AlterarStatus('PRONTO_PARA_RETIRADA')} className='dropdown-item'>Pronto para retirada</a></li>
+            <li><a href='#entrega' onClick={() => AlterarStatus('SAIU_PARA_ENTREGA')} className='dropdown-item'>Saiu para Entrega</a></li>
+            <li><a href='#recebido' onClick={() => AlterarStatus('RECEBIDO')} className='dropdown-item'>Recebido</a></li>
+            <li><a href='#finalizado' onClick={() => AlterarStatus('FINALIZADO')} className='dropdown-item'>Finalizado</a></li>
+            <li><a href='#cancelado' onClick={() => AlterarStatus('CANCELADO')} className='dropdown-item'>Cancelado</a></li>
+          </ul>
+        </div>
       </div>
     </div>
-
-  </div>
+  );
 }
-
-/*
-    await fetch('https://exp.host/--/api/v2/push/send', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Accept-encoding': 'gzip, deflate',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(message),
-    });
-*/
-
-/*
-  async function SendNotification(expoPushToken, pedido_id, codigo_status) {
-    const message = {
-      "AuthenticationKey": expoPushToken,
-      "payload": {
-        "to": "Pedidos",
-        // "collapse_key" : "type_a",
-        "notification": {
-          "title": "DeliveryBairro.com",
-          "body": "Pedido #"+pedido_id+" atualizado em "+new Date().toLocaleString(),
-          "data": {"PedidoID": pedido_id, "Status": codigo_status},
-          "sound": "default",
-          "priority": "high",
-          "color": "#1A73E8",
-          // "tag": "app"
-        }
-      }
-    };
-
-    await fetch("https://fcm.googleapis.com/fcm/send", {
-      method: "POST",
-      referrerPolicy: "strict-origin-when-cross-origin",
-      mode: "no-cors",
-      headers: {
-        'Accept': '*',
-        "Accept-encoding": "gzip, deflate",
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(message),
-    });
-  }
-
-*/
--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+/**
+* src/components/menu/index.jsx
+*/
+
+import React, { useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import './index.css';
 
-import logomarca from '../../assets/logomarca.png';
+import imgLogo from '../../assets/logomarca.png';
+import imgLogoMini from '../../assets/logo.png';  // nova imagem menor
 
 function Menu(props) {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   const activeLink    = "nav-link active";
   const inactiveLink  = "nav-link text-white";
 
@@ -19,13 +36,12 @@ function Menu(props) {
     
       <div className="d-flex flex-column align-items-center align-items-sm-start px-2 pt-2 text-white min-vh-100">    {/* <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark"> */}
         <a href="/" onClick={LougOut} className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-          <img src={logomarca} className="img-logo" width="180px" alt="logo" /><br/>
+          <img src={isMobile ? imgLogoMini : imgLogo} className="img-logo" alt="logo" /><br/>
         </a>
         <p></p>
-        <span class="fs-4">Menu Principal</span>
-        <span></span>
+        <span className="fs-4">{isMobile ? 'Menu' : 'Menu Principal'}</span>
 
-        <ul class="nav nav-pills flex-column mb-auto">
+        <ul className="nav nav-pills flex-column mb-auto">
           <hr/>
           <li className="nav-item">
             <Link to="/app/pedidos" className={props.page === "pedidos" ? activeLink : inactiveLink} aria-current="page">
@@ -41,7 +57,7 @@ function Menu(props) {
 
           <li>
             <Link to="/app/extras" className={props.page === "extras" ? activeLink : inactiveLink}>
-              <i class="fs-4 bi-paperclip"></i> <span className="ms-1 d-none d-sm-inline">Acréscimos</span>
+              <i className="fs-4 bi-paperclip"></i> <span className="ms-1 d-none d-sm-inline">Acréscimos</span>
             </Link>
           </li>
 
@@ -50,11 +66,11 @@ function Menu(props) {
               <i className="fs-4 bi-wallet2"></i> <span className="ms-1 d-none d-sm-inline">Delivery</span>
             </Link>
           </li>
-          <li>
+          {/* <li>
             <Link to="/app/config" className={props.page === "config" ? activeLink : inactiveLink}>
               <i className="fs-4 bi-gear"></i> <span className="ms-1 d-none d-sm-inline">Configurações</span>
             </Link>
-          </li>
+          </li> */}
           <hr/>
           <li>
             <Link to="/" onClick={LougOut} className={props.page === "logout" ? activeLink : inactiveLink}>
@@ -68,61 +84,3 @@ function Menu(props) {
 }
 
 export default Menu;
-
-/**
-  <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px;">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-      <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-      <span class="fs-4">Sidebar</span>
-    </a>
-    <hr>
-    <ul class="nav nav-pills flex-column mb-auto">
-      <li class="nav-item">
-        <a href="#" class="nav-link active" aria-current="page">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
-          Home
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link text-white">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
-          Dashboard
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link text-white">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
-          Orders
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link text-white">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
-          Products
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link text-white">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
-          Customers
-        </a>
-      </li>
-    </ul>
-    <hr>
-    <div class="dropdown">
-      <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-        <strong>mdo</strong>
-      </a>
-      <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-        <li><a class="dropdown-item" href="#">New project...</a></li>
-        <li><a class="dropdown-item" href="#">Settings</a></li>
-        <li><a class="dropdown-item" href="#">Profile</a></li>
-        <li><hr class="dropdown-divider"></li>
-        <li><a class="dropdown-item" href="#">Sign out</a></li>
-      </ul>
-    </div>
-  </div>
-
- */
-
