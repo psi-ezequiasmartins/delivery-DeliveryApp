0f425a34f174637d3da4ea8a947db81dbe6ee347
Ezequias <ezequiasmartins@gmail.com>
2024-06-07 16:14:42 -0300

    07062024-1608

--- src/site/pages/mobile.jsx
+++ src/site/pages/mobile.jsx
@@ -10,8 +10,8 @@ function Mobile() {
               <div className="bloco">
                 <h1>Baixe grátis aqui o nosso <br/>aplicativo Mobile (Android/iOs*)</h1>
                 <h4>Acesse o nosso catálogo de estabelecimentos com entregas a domicílio próximo a você!</h4>
-                <a href="https://drive.google.com/file/d/1jQ--oS23rIcbtoVKiyXdJ5dALYW_-Ksj/view?usp=sharing" type="button" className="btn btn-dark btn-lg btn-app">Download | DeliveryBairro UserApp v1.0 <i className="fa fa-android"></i><br/>
-                BUILD 26042024-1647</a>
+                <a href="https://drive.google.com/file/d/1jQ--oS23rIcbtoVKiyXdJ5dALYW_-Ksj/view?usp=sharing" type="button" className="btn btn-dark btn-lg btn-app">Download | DeliveryBairro UserApp v1.3 <i className="fa fa-android"></i><br/>
+                BUILD 07062024-1608</a>
               </div>
               <p className="text-white">(*) em breve disponível também para dispositivos iOs (iPhone, iPads, etc)</p>
             </div>
