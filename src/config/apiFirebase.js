478f18628347539693b34e89fa7bcc3e6ba93543
Ezequias <ezequiasmartins@gmail.com>
2025-03-22 23:22:21 -0300

    22032025-2322

--- src/config/apiFirebase.js
+++ src/config/apiFirebase.js
@@ -5,17 +5,26 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVZPJtP2wBvuOXEHYGETlysuE_8kwYLs4",
-  authDomain: "psi-deliverybairro.firebaseapp.com",
-  databaseURL: "https://psi-deliverybairro-default-rtdb.firebaseio.com",
-  projectId: "psi-deliverybairro",
-  storageBucket: "psi-deliverybairro.firebasestorage.app",
-  messagingSenderId: "234983243246",
-  appId: "1:234983243246:web:eb96adf25d986a6292a2ff",
-  measurementId: "G-68L43T7X7W"
+  apiKey: "AIzaSyC0v2LQ4ZSidxrUECbk9mIkPBXEBtpcpaw",
+  authDomain: "psi-crm-ca846.firebaseapp.com",
+  databaseURL: "https://psi-crm-ca846-default-rtdb.firebaseio.com",
+  projectId: "psi-crm-ca846",
+  storageBucket: "psi-crm-ca846.appspot.com",
+  messagingSenderId: "862930671910",
+  appId: "1:862930671910:web:e0ff5160f50d67a974c9e7"
 };
 
 export const firebase_app = initializeApp(firebaseConfig);
+  // apiKey: "AIzaSyBVZPJtP2wBvuOXEHYGETlysuE_8kwYLs4",
+  // authDomain: "psi-deliverybairro.firebaseapp.com",
+  // databaseURL: "https://psi-deliverybairro-default-rtdb.firebaseio.com",
+  // projectId: "psi-deliverybairro",
+  // storageBucket: "psi-deliverybairro.firebasestorage.app",
+  // messagingSenderId: "234983243246",
+  // appId: "1:234983243246:web:eb96adf25d986a6292a2ff",
+  // measurementId: "G-68L43T7X7W"
+
+
   // apiKey: process.env.API_KEY,
   // authDomain: process.env.AUTH_DOMAIN,
   // projectId: process.env.PROJECT_ID,
