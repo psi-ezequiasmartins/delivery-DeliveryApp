f4071afbe5a1d35cee5c7658b3b8f744feacbf4f
Ezequias <ezequiasmartins@gmail.com>
2025-03-22 23:17:11 -0300

    22032025-2316

--- src/config/apiFirebase.js
+++ src/config/apiFirebase.js
@@ -5,13 +5,21 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  apiKey: "AIzaSyBVZPJtP2wBvuOXEHYGETlysuE_8kwYLs4",
+  authDomain: "psi-deliverybairro.firebaseapp.com",
+  databaseURL: "https://psi-deliverybairro-default-rtdb.firebaseio.com",
+  projectId: "psi-deliverybairro",
+  storageBucket: "psi-deliverybairro.firebasestorage.app",
+  messagingSenderId: "234983243246",
+  appId: "1:234983243246:web:eb96adf25d986a6292a2ff",
+  measurementId: "G-68L43T7X7W"
 };
 
 export const firebase_app = initializeApp(firebaseConfig);
+  // apiKey: process.env.API_KEY,
+  // authDomain: process.env.AUTH_DOMAIN,
+  // projectId: process.env.PROJECT_ID,
+  // storageBucket: process.env.STORAGE_BUCKET,
+  // messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  // appId: process.env.APP_ID,
+  // measurementId: process.env.MEASUREMENT_ID
