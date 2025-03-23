fc3f385f65d69016f3dec76bbf957da2ef2ece26
Ezequias <ezequiasmartins@gmail.com>
2025-03-23 00:23:25 -0300

    23032025-0023

--- src/config/apiFirebase.js
+++ src/config/apiFirebase.js
@@ -12,7 +12,6 @@ const firebaseConfig = {
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
 };
 
 export const firebase_app = initializeApp(firebaseConfig);
