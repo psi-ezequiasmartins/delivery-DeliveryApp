75b7e4fd4c133e17f3bbfebe6de607d310034288
Ezequias Martins <ezequiasmartins@gmail.com>
2025-04-28 00:55:04 -0300

    28042025-005500

--- src/config/apiAxios.js
+++ src/config/apiAxios.js
@@ -3,7 +3,7 @@
  */
 
 import Axios from "axios";
-const URL = process.env.REACT_APP_BASE_URL; // "http://192.168.0.9:3357"; // "http://localhost:3357"; 
+const URL = process.env.REACT_APP_BASE_URL; // "http://192.168.0.2:3357"; // "http://localhost:3357"; 
 
 const api = Axios.create({
   baseURL: URL,
